d652b45f63c4db48f6b65cb4dbd75e97b671f68c
RichardSousa00 <68669543+RichardSousa00@users.noreply.github.com>
2022-06-04 16:15:43 -0300

    save

--- dinastia_website/src/pages/home.js
+++ dinastia_website/src/pages/home.js
@@ -4,7 +4,30 @@ import '../App.css';
 const HomePage = () => {
     return (
         <div>
-            teste
+            <div style={{height: '30vh', alignItems: 'center', alignText: 'center'}}>
+                Texto introdutório
+
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-around', height: '30vh', alignItems: 'center', marginTop: '10vh' }}>
+                <div style={{
+                    border: 'solid', height: '30vh', width: '25vw', alignItems: 'center', marginLeft: '5vw',
+                    borderWidth: '.5px'
+                }}>
+                    Card1
+                </div>
+                <div style={{
+                    border: 'solid', height: '30vh', width: '25vw', alignItems: 'center', marginLeft: '5vw',
+                    borderWidth: '.5px'
+                }}>
+                    Card2
+                </div>
+                <div style={{
+                    border: 'solid', height: '30vh', width: '25vw', alignItems: 'center', marginLeft: '5vw',
+                    marginRight: '5vw', borderWidth: '.5px'
+                }}>
+                    Card3
+                </div>
+            </div>
         </div>
     )
 }
