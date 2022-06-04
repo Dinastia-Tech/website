2ca4b8bb49b3d1f6dd44394678b80c6d02287dc2
BeahIF <beatrizdacosta1@gmail.com>
2022-06-04 14:49:34 -0300

    tentando criar uma lista de empresas

--- dinastia_website/src/pages/home.js
+++ dinastia_website/src/pages/home.js
@@ -2,14 +2,42 @@ import logo from '../logo.svg';
 import '../App.css';
 
 const HomePage = () => {
+  
+  function renderProduct({item: product}) {
     return (
-        <div style={{backgroundColor: 'gray'}}>
-            <p>
-              Página Teste .
-            </p>
+      <Product {...product} 
+      onPress={() => {
+        navigation.navigate('ProductDetails', {
+          productId: product.id,
+        });
+      }}
+      />
+    );
+  }
+  
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    setProducts(getProducts());
+  });
+  
+  return (
+    <FlatList
+      style={styles.productsList}
+      contentContainerStyle={styles.productsListContainer}
+      keyExtractor={(item) => item.id.toString()}
+      data={products}
+      renderItem={renderProduct}
+    />
+  );
+    // return (
+    //     <div style={{backgroundColor: 'gray'}}>
+    //         <p>
+    //           Página Teste .
+    //         </p>
           
-        </div>
-      )
+    //     </div>
+    //   )
 }
 
 export default HomePage
